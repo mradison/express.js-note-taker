31c93d36d3bee56c68fe22f4fe108b06086779d2
madison reynolds <madison.reynolds@phoenix.edu>
2023-11-15 11:04:17 -0700

    added code to routes file and server.js file so that it will start working to retrieve data from API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,30 @@
 const router = require('express').Router()
 const path = require('path')
+const fs = require('fs').promises;
+const { isUtf8 } = require('buffer');
 
 app.get('/api/notes', (req,res) => {
+
+    fs.readFile('db/db.json', 'utf8') .then(data => {
+        res.json(data);
+    })
+
+
     // call a function that I write outside of this to get the notes from the db.json file
-    res.json()
+   
 })
 app.post('/api/notes', (req,res) => {
     // recieve the data
 
     req.body
 
+    fs.writeFile(`./db/${newReview.product}.json`, reviewString, (err) =>
+    err
+      ? console.error(err)
+      :
     //call a function that will write the user's notes inside a db.json file
     res.json('Note was added')
+    )
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/apiRoutes')
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3004
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use('/', htmlRoutes )
+app.use('/', apiRoutes  )
 
 app.listen(PORT, () => console.log('http://localhost:'+ PORT))
